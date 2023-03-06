d88a9d1bcdce2ba72a367ef1af1b632f3983f8e0
Ercan Bagra <ercanbagra@hotmail.com>
2023-03-07 00:47:22 +0100

    init app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { HomeScreen } from './src/screens/home/home.screen';
+
+function App() {
+  return (
+    <SafeAreaView >
+      <HomeScreen />
+    </SafeAreaView>
+  );
+}
+export default App;
